3b5a14aa5de0016880cd4537ee7bbc1ed6a44a0f
Vova <gusarday@mail.ru>
2022-01-20 12:33:34 +0700

    init game 2

---
+++ pudge.cpp
@@ -0,0 +1,9 @@
+#include "pudge.h"
+
+Pudge::Pudge()
+{
+    this->health = 150;
+    this->armor = 12;
+    this->damage = 15;
+    strcpy(this->name, "Pudge");
+}
---
+++ pudge.h
@@ -0,0 +1,10 @@
+#ifndef PUDGE_H
+#define PUDGE_H
+#include "unit.h"
+
+class Pudge : public Unit {
+public:
+    Pudge();
+};
+
+#endif // PUDGE_H
